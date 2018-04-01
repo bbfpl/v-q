90787d6b8b3ce528aa9db2ed6ceba2f10f399848
bobby <bobofpl@gmail.com>
2018-04-02 00:22:04 +0800

    2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,75 @@
 <template>
-  <div class="hello">
-    Home
-  </div>
+ 
+<div class="container">
+	<div class="row hidden">
+		<div class="col-12">
+			<div class="demo flex direction-vertical justify-center">
+				<p>1</p>
+				<p>2</p>
+			</div>
+		</div>
+		<div class="col-12">
+			<div class="demo">
+				<p>1</p>
+				<p>2</p>
+			</div>
+		</div>
+		<div class="col-12">
+			<div class="demo">
+				<p>1</p>
+				<p>2</p>
+			</div>
+		</div>
+		<div class="col-12">
+			<div class="demo">
+				<p>1</p>
+				<p>2</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-6">
+			<div class="demo flex justify-around">
+				<span>1</span>
+				<span>1</span>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="demo">2</div>
+		</div>
+		<div class="col-6">
+			<div class="demo">3</div>
+		</div>
+		<div class="col-6">
+			<div class="demo">4</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<div class="demo">
+				<span>1</span>
+			</div>
+		</div>
+		<div class="col">
+			<div class="demo">2</div>
+		</div>
+		<div class="col">
+			<div class="demo">3</div>
+		</div>
+		<div class="col">
+			<div class="demo">4</div>
+		</div>
+	</div>
+</div>
+ 
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    return { 
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import './style/ui.css'
-import './style/main.css'
+import '@/style/ui.css'
+import '@/style/main.css'
 
 Vue.config.productionTip = false
 
